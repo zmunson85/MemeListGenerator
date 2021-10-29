88160fdddcd5ca00c764b28529aa719c5ae5f518
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-10-28 18:22:48 -0600

    start code

--- meme-list-generator/src/index.js
+++ meme-list-generator/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MemeGenerator from './MemeGenerator';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<MemeGenerator />,document.getElementById('root')
 );
 
 
