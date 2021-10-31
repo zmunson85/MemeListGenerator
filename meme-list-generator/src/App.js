8335556de424f2a9a759aaba34f2818d39dd6878
Zach Munson <zmunson85@gmail.com>
2021-10-31 12:04:33 -0600

    'Revision'

--- meme-list-generator/src/App.js
+++ meme-list-generator/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import MemeGenerator from './MemeGenerator';
-
+// import List from './List'
+//maintain state here and pass the array to list , handle submit to meme generator
 const App = () => {
   return (
     <>
--- meme-list-generator/src/MemeGenerator.js
+++ meme-list-generator/src/MemeGenerator.js
@@ -1,62 +1,73 @@
 import React, { useState, useEffect } from "react";
 
-function MemeGenerator() {
-    const [upperText, setUpperText] = useState("")
-    const [lowerText, setLowerText] = useState("")
-    const [allMemes, setAllMemes] = useState("")
-    const [randomMeme, setRandomMeme] = useState(
-        "http://i.imgflip.com/2fm6x.jpg"
-    );
 
-    const fetchMeme = () => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then((response) => response.json())
-            .then((response) => setAllMemes(response.data.memes))
-            .catch()
+function MemeGenerator() {
+    const [memes, setMemes] = useState([])
+    const [memeIndex, setMemeIndex] = useState(0);
+    const [captions, setCaptions] = useState([]);
+
+    const randomMeme = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * i);
+            const temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
     };
+    const generateMeme = () => {
+        return (
+            <>
+                <h1 style={{ color: 'red' }}>{captions}</h1>
+            </>
+        )
+    };
+
 
     useEffect(() => {
-        fetchMeme()
-    }, [])
+        fetch("https://api.imgflip.com/get_memes").then(res => res.json()).then(res => {
+            const memes = res.data.memes;
+            randomMeme(memes);
+            setMemes(memes);
+        })
+    }, []);
 
-    const getRandomMeme = (event) => {
-        event.preventDefault()
-        const randomNum = Math.floor(Math.random() * Math.floor(allMemes.length));
-        setRandomMeme(allMemes[randomNum].url);
+    useEffect(() => {
+        if (memes.length) {
+            console.log(memes[memeIndex].box_count.value)
+            setCaptions(Array(memes[memeIndex].box_count).fill(''));
+        }
+    }, [memeIndex, memes]);
+
+    const updateCaption = (e, index) => {
+        const text = e.target.value || '';
+        setCaptions(
+            captions.map((box_count, i) => {
+                if (index === i) {
+                    console.log(text)
+                    return text;
+                } else {
+                    console.log(box_count.value)
+                    return box_count;
+                }
+            })
+        );
     };
 
-    const handleTop = (event) => {
-        setUpperText(event.target.value)
-    };
-    const handleBottom = (event) => {
-        setLowerText(event.target.value)
-    };
     return (
-        <div>
-            <form onSubmit={getRandomMeme} className="meme-form">
-                <input
-                    type="text"
-                    name="upperText"
-                    placeholder="top text"
-                    value={upperText}
-                    onChange={handleTop}
-                />
-                <input
-                    type="text"
-                    name="lowerText"
-                    placeholder="bottom text"
-                    value={lowerText}
-                    onChange={handleBottom}
-                />
-                <button>Generate Meme</button>
-            </form>
-            <div className="meme">
-                <img src={randomMeme} alt={upperText} />
-                <h2 className="top">{upperText}</h2>
-                <h2 className="bottom">{lowerText}</h2>
-            </div>
-        </div>
-    );
+        memes.length ?
+            <div className='container'>
+                <button onClick={(generateMeme)} className='generateNewMeme'>Generate</button>
+                <button onClick={() => setMemeIndex(memeIndex + 1)} className='skipButton'>Refresh</button>
+                {
+                    captions.map((c, index) => (
+                        <input onChange={(e) => updateCaption(e, index)} key={index} />
+                    ))
+                }
+
+                <img src={memes[memeIndex].url} alt={{ captions }} />
+
+            </div> : <></>
+    )
 }
 
 export default MemeGenerator;
