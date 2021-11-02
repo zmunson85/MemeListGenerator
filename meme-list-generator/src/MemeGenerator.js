b7ed942c3d10553709dafd670fc03ada9cc5c303
Zach Munson <zmunson85@gmail.com>
2021-11-02 12:29:46 -0600

    'cleanTree'

--- meme-list-generator/src/MemeGenerator.js
+++ meme-list-generator/src/MemeGenerator.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Header from "./Header";
 
 
 function MemeGenerator() {
@@ -7,27 +6,15 @@ function MemeGenerator() {
     const [generatedMemes, setGeneratedMemes] = useState([])
     const [memeIndex, setMemeIndex] = useState(0);
     const [captions, setCaptions] = useState([]);
-<<<<<<< HEAD
-
-=======
->>>>>>> 594acf750f7d7337e952867957ba659ddada33bd
     const generateMeme = (e) => {
         e.preventDefault();
         const currentMeme = memes[memeIndex];
         const formData = new FormData();
-<<<<<<< HEAD
-
-=======
->>>>>>> 594acf750f7d7337e952867957ba659ddada33bd
         formData.append('username', 'VschoolTesting');
         formData.append('password', 'Testing1');
         formData.append('template_id', currentMeme.id);
         captions.map((c, index) => formData.append(`boxes[${index}][text]`, c));
         console.log(generatedMemes)
-<<<<<<< HEAD
-
-=======
->>>>>>> 594acf750f7d7337e952867957ba659ddada33bd
         fetch('https://api.imgflip.com/caption_image', {
             method: 'POST',
             body: formData
@@ -56,11 +43,6 @@ function MemeGenerator() {
             setCaptions(Array(memes[memeIndex].box_count).fill(''));
         }
     }, [memeIndex, memes]);
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 594acf750f7d7337e952867957ba659ddada33bd
     const updateCaption = (e, index) => {
         const text = e.target.value || '';
         setCaptions(
@@ -74,21 +56,10 @@ function MemeGenerator() {
         );
     };
     return (
-       
+
         memes.length ?
-       <><div className="header"><h1>Meme Generator</h1></div><div className='container'>
+            <><div className="header"><h1>Meme Generator</h1></div><div className='container'>
                 <form onSubmit={generateMeme}>
-<<<<<<< HEAD
-                    {
-                        captions.map((c, index) => (
-                            <input onChange={(e) => updateCaption(e, index)} key={index} required />
-                        ))
-                    }
-                    <img src={memes[memeIndex].url} alt='meme' />
-                    {captions.map((inputText, caption) => {
-                        console.log(inputText)
-                        return <h1 key={caption}>{inputText}</h1>
-=======
                     {captions.map((c, index) => (
                         <input onChange={(e) => updateCaption(e, index)} key={index} required />
                     ))}
@@ -96,26 +67,14 @@ function MemeGenerator() {
                     {captions.map((inputText, caption) => {
                         console.log(inputText);
                         return <h2 key={caption}>{inputText}</h2>;
->>>>>>> 594acf750f7d7337e952867957ba659ddada33bd
                     })}
                     <button className='generateButton'>Generate</button>
                     <button onClick={() => setMemeIndex(memeIndex + 1)} className='skipButton'>Refresh</button>
                 </form>
-<<<<<<< HEAD
-                {generatedMemes.length ?
-                    <div className='previewMeme'>
-                        <img src={generatedMemes} alt='test' />
-                    </div>
-                    : null
-                }
-                {console.log(generatedMemes)}
-            </div> : <> </>
-=======
                 <div className='previewMeme'>
                     <img src={generatedMemes} alt='test' />
                 </div>
             </div></> : <> </>
->>>>>>> 594acf750f7d7337e952867957ba659ddada33bd
     )
 }
 export default MemeGenerator;
