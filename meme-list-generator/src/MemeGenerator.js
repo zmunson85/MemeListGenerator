5db67239d38713a7dbc57aa6b8bd3ab40b9d3a1b
Zach Munson <zmunson85@gmail.com>
2021-11-02 12:40:43 -0600

    'update'

--- meme-list-generator/src/MemeGenerator.js
+++ meme-list-generator/src/MemeGenerator.js
@@ -6,15 +6,18 @@ function MemeGenerator() {
     const [generatedMemes, setGeneratedMemes] = useState([])
     const [memeIndex, setMemeIndex] = useState(0);
     const [captions, setCaptions] = useState([]);
+
     const generateMeme = (e) => {
         e.preventDefault();
         const currentMeme = memes[memeIndex];
         const formData = new FormData();
+
         formData.append('username', 'VschoolTesting');
         formData.append('password', 'Testing1');
         formData.append('template_id', currentMeme.id);
         captions.map((c, index) => formData.append(`boxes[${index}][text]`, c));
         console.log(generatedMemes)
+
         fetch('https://api.imgflip.com/caption_image', {
             method: 'POST',
             body: formData
@@ -26,6 +29,8 @@ function MemeGenerator() {
             });
         });
     };
+
+
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes").then(res => res.json()).then(res => {
             const memes = res.data.memes;
@@ -38,11 +43,14 @@ function MemeGenerator() {
             setMemes(memes);
         });
     }, []);
+
     useEffect(() => {
         if (memes.length) {
             setCaptions(Array(memes[memeIndex].box_count).fill(''));
         }
     }, [memeIndex, memes]);
+
+
     const updateCaption = (e, index) => {
         const text = e.target.value || '';
         setCaptions(
@@ -55,26 +63,35 @@ function MemeGenerator() {
             })
         );
     };
-    return (
 
+
+    return (
         memes.length ?
-            <><div className="header"><h1>Meme Generator</h1></div><div className='container'>
+            <div className='container'>
+
+
                 <form onSubmit={generateMeme}>
-                    {captions.map((c, index) => (
-                        <input onChange={(e) => updateCaption(e, index)} key={index} required />
-                    ))}
+                    {
+                        captions.map((c, index) => (
+                            <input onChange={(e) => updateCaption(e, index)} key={index} required />
+                        ))
+                    }
                     <img src={memes[memeIndex].url} alt='meme' />
                     {captions.map((inputText, caption) => {
-                        console.log(inputText);
-                        return <h2 key={caption}>{inputText}</h2>;
+                        console.log(inputText)
+                        return <h1 key={caption}>{inputText}</h1>
                     })}
                     <button className='generateButton'>Generate</button>
                     <button onClick={() => setMemeIndex(memeIndex + 1)} className='skipButton'>Refresh</button>
                 </form>
-                <div className='previewMeme'>
-                    <img src={generatedMemes} alt='test' />
-                </div>
-            </div></> : <> </>
+                {generatedMemes.length ?
+                    <div className='previewMeme'>
+                        <img src={generatedMemes} alt='test' />
+                    </div>
+                    : null
+                }
+            </div> : <> </>
     )
 }
+
 export default MemeGenerator;
