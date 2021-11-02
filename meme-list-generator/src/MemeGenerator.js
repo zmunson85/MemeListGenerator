820fd4ff0e3fb77a8ba99c93947f398197c61fcc
Diana I <89826035+dibarra721@users.noreply.github.com>
2021-11-02 09:54:27 -0600

    initial commit

--- meme-list-generator/src/MemeGenerator.js
+++ meme-list-generator/src/MemeGenerator.js
@@ -5,6 +5,13 @@ function MemeGenerator() {
     const [memes, setMemes] = useState([])
     const [memeIndex, setMemeIndex] = useState(0);
     const [captions, setCaptions] = useState([]);
+    const [generatedMeme, setGeneratedMeme] = useState([{
+        box_count: 0,
+        topText: '',
+        bottomText: '',
+        url: ''
+
+    }]);
 
     const randomMeme = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -14,18 +21,13 @@ function MemeGenerator() {
             array[j] = temp;
         }
     };
-    const generateMeme = () => {
-        return (
-            <>
-                <h1 style={{ color: 'red' }}>{captions}</h1>
-            </>
-        )
-    };
+
 
 
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes").then(res => res.json()).then(res => {
-            const memes = res.data.memes;
+            const memes = res.data.memes.box_count;
+            console.log(memes)
             randomMeme(memes);
             setMemes(memes);
         })
@@ -33,41 +35,4 @@ function MemeGenerator() {
 
     useEffect(() => {
         if (memes.length) {
-            console.log(memes[memeIndex].box_count.value)
-            setCaptions(Array(memes[memeIndex].box_count).fill(''));
-        }
-    }, [memeIndex, memes]);
-
-    const updateCaption = (e, index) => {
-        const text = e.target.value || '';
-        setCaptions(
-            captions.map((box_count, i) => {
-                if (index === i) {
-                    console.log(text)
-                    return text;
-                } else {
-                    console.log(box_count.value)
-                    return box_count;
-                }
-            })
-        );
-    };
-
-    return (
-        memes.length ?
-            <div className='container'>
-                <button onClick={(generateMeme)} className='generateNewMeme'>Generate</button>
-                <button onClick={() => setMemeIndex(memeIndex + 1)} className='skipButton'>Refresh</button>
-                {
-                    captions.map((c, index) => (
-                        <input onChange={(e) => updateCaption(e, index)} key={index} />
-                    ))
-                }
-
-                <img src={memes[memeIndex].url} alt={{ captions }} />
-
-            </div> : <></>
-    )
-}
-
-export default MemeGenerator;
+            console.log(memes[memeIndex].box_count)
